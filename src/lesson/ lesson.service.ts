730ac50f4dba9a150bbfb629d26884404234021a
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-10-25 11:31:18 +0300

    ➕ LessonService and createLesson method

---
+++ src/lesson/ lesson.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Lesson } from './lesson.entity';
+import { Repository } from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+
+@Injectable()
+export class LessonService {
+  constructor(
+    @InjectRepository(Lesson)
+    private readonly lessonRepository: Repository<Lesson>,
+  ) {}
+
+  async createLesson(
+    name: string,
+    startDate: string,
+    endDate: string,
+  ): Promise<Lesson> {
+    return this.lessonRepository.save({
+      id: uuidV4(),
+      name,
+      startDate,
+      endDate,
+    });
+  }
+}
