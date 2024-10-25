3b6fe7cafe8dc53d1e1217ca3fa1f31f594dccfe
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-10-25 16:07:35 +0300

    ➕ Get All Students GraphQL Query

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -12,6 +12,10 @@ export class StudentService {
     private readonly studentRepository: Repository<Student>,
   ) {}
 
+  async getStudents(): Promise<Student[]> {
+    return this.studentRepository.find();
+  }
+
   async createStudent(
     createStudentInput: CreateStudentInput,
   ): Promise<Student> {
